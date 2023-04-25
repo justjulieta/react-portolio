0392dc8504e37b206af8e2bf8b4e8240f0b1a3df
Julieta Ramirez Solis Nguyen <julieta.rammirez27@gmail.com>
2023-04-25 01:28:29 -0700

    Changes to be committed:

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,108 +1,83 @@
 import React, { useState } from "react";
-import { validateEmail } from "../utils/helpers";
+import Typography from '@mui/material/Typography';
 
-function Contact() {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+const ContactForm = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
 
-  const [errors, setErrors] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const { name, email, message } = formState;
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setFormState({ ...formState, [name]: value });
-
-    if (name === "email") {
-      const isValid = validateEmail(value);
-      setErrors({ ...errors, email: isValid ? "" : "Invalid email" });
-    } else {
-      setErrors({ ...errors, [name]: value ? "" : `${name} is required` });
-    }
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (validateForm(errors)) {
-      console.log("Valid form data:", formState);
-      setFormState({ name: "", email: "", message: "" });
-    } else {
-      console.log("Invalid form data:", errors);
-    }
-  }
-
-  function validateForm(errors) {
-    return Object.values(errors).every((e) => e === "");
+  if (submitted) {
+    return (
+      <>
+        <h2>Thank you!</h2>
+        <div>We'll be in touch soon.</div>
+      </>
+    );
   }
 
   return (
-    <section className="bg-gray-200 p-8">
-      <div className="text-center">
-        <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
+    <form style={{ backgroundColor: '#1f1f1f', display: 'block', color: '#fff', padding: '1rem', borderRadius: '0.5rem',}} 
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+    >
+      <Typography variant="h2" style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: '16px',
+        fontWeight: 'bold',
+        fontSize: '24px',
+        color: '#ffffff',}}>
+            Contact Us
+        </Typography>
+      <div>
+        <input style={{ 
+          backgroundColor: '#3d3d3d', 
+          color: '#ffff', 
+          border: 'none', 
+          padding: '0.5rem', 
+          borderRadius: '0.25rem', 
+          width: '95%', 
+          marginBottom: '1rem'}} 
+          type="text" placeholder="Your name" name="name" required />
       </div>
       <div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="name" className="font-bold mb-2 block">
-              Name:
-            </label>
-            <textarea
-              style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-              outline="none"
-              type="text"
-              value={name}
-              onChange={handleChange}
-              name="name"
-              className="w-full p-2 border rounded"
-            />
-            {errors.name && <p className="text-red-500 mt-2">{errors.name}</p>}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="font-bold mb-2 block">
-              Email address:
-            </label>
-            <textarea
-              style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-              type="email"
-              value={email}
-              onChange={handleChange}
-              name="email"
-              className="w-full p-2 border rounded"
-            />
-            {errors.email && <p className="text-red-500 mt-2">{errors.email}</p>}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="font-bold mb-2 block">
-              Message:
-            </label>
-            <textarea
-              name="message"
-              value={message}
-              onChange={handleChange}
-              rows="5"
-              className="w-full p-2 border rounded"
-            />
-            {errors.message && (
-              <p className="text-red-500 mt-2">{errors.message}</p>
-            )}
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Submit
-          </button>
-        </form>
+        <input style={{ 
+          backgroundColor: '#3d3d3d', 
+          color: '#fff', border: 'none', 
+          padding: '0.5rem', 
+          borderRadius: '0.25rem', 
+          width: '95%', 
+          marginBottom: '1rem'}} 
+          type="email" placeholder="Email" name="email" required />
       </div>
-    </section>
+      <div>
+        <textarea style={{ 
+          backgroundColor: '#3d3d3d', 
+          color: '#fff', 
+          border: 'none', 
+          padding: '0.5rem', 
+          borderRadius: '0.25rem', 
+          width: '95%', 
+          marginBottom: '1rem'}} 
+          placeholder="Your message" name="message" required />
+      </div>
+      <div>
+        <button style={{ 
+          backgroundColor: '#1a1a1a', 
+          color: '#fff', 
+          border: 'none', 
+          padding: '0.5rem', 
+          borderRadius: '0.25rem', 
+          cursor: 'pointer',}} 
+          type="submit">Submit</button>
+      </div>
+    </form>
   );
-}
+};
 
-export default Contact;
+export default ContactForm;
